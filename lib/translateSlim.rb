dd08b6374b92f53042c36ac8f1acb79a763ef16b
sacry-dyn <matthias.nitsche@dynport.de>
2014-07-15 11:17:16 +0200

    added support for unix_diff_mode

--- lib/translateSlim.rb
+++ lib/translateSlim.rb
@@ -4,21 +4,43 @@ require './parser'
 
 class TranslateSlim
 
-  def initialize(file, output=nil, diff_mode=false)
-    @bak_path, @file_path, @content = create_backup(file)
+  def initialize(options={})
+    @options = options
+    @bak_path, @file_path, @content = create_backup(@options)
     @key_base = translation_key_base
     @new_content = []
   end
 
   def unix_diff_mode
+    @content.each do |old_line|
+      word = Word.new(old_line, @key_base)
+      matcher = Matcher.match(word)
+      if not matcher.empty? then
+        new_line, translations = Transformer.new(word).transform
+        if translations_are_invalid?(translations)
+          @new_content << old_line
+        else
+          @new_content << new_line
+        end
+      else @new_content << old_line end
+    end
+    process_unix_diff
+  end
 
+  def process_unix_diff
+    FileWriter.write(@file_path, @new_content.join("\n"))
+    ConsolePrinter.unix_diff(@bak_path, @file_path)
+    if IOAction.yes_or_no?("Do you like what you see?") then
+      puts "Have a nice day!"
+    else
+      FileWriter.write(@file_path, @content.join("\n"))
+    end
   end
 
   def stream_mode
-    puts "content=#{@content}"
     @content.each_with_index do |old_line, idx|
 
-      word = Word.new(old_line, idx, @key_base)
+      word = Word.new(old_line, @key_base)
       matcher = Matcher.match(word)
 
       if not matcher.empty? then
@@ -34,7 +56,6 @@ class TranslateSlim
       else update_with(idx, old_line) end
 
       FileWriter.append(@file_path, @new_content[idx])
-
     end
   end
 
@@ -59,17 +80,27 @@ class TranslateSlim
     "#{dirname}.#{fname}"
   end
 
-  def create_backup(file)
-    original_file_path = FileUtils.abs_path(file)
+  def create_backup(options)
+    original_file_path = FileUtils.abs_path(options[:input])
     content = FileReader.read(original_file_path)
     bak_path = "#{original_file_path}.bak"
     File.open(bak_path, "w"){|f| f.write(content)}
-    FileWriter.overwrite(original_file_path)
-    [bak_path, original_file_path, content.split("\n")]
+    new_file_path = build_new_file_path(options)
+    FileWriter.overwrite(new_file_path)
+    [bak_path, new_file_path, content.split("\n")]
+  end
+
+  def build_new_file_path(options)
+    out = options[:output]
+    if out and File.file?(out) then
+      FileUtils.abs_path(out)
+    else
+      FileUtils.abs_path(options[:input])
+    end
   end
 end
 
-CommandLine.new(ARGV).main
+CommandLine.new.main
 
 
 
