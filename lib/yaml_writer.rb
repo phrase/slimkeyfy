d8b4651d1932d1d7bb8f4e71452ad6bb028530ec
sacry-dyn <matthias.nitsche@dynport.de>
2014-07-15 12:01:11 +0200

    simple yaml_writer to merge translation_hashes with current yaml files

--- lib/yaml_writer.rb
+++ lib/yaml_writer.rb
@@ -1,4 +1,36 @@
+require 'yaml' 
+require './file_utils'
 
 class YamlWriter
 
+  attr_reader :translation_hash, :yaml_hash
+
+  def initialize(yaml_file_path, translation_hash, locale="en")
+    @yaml_file_path = yaml_file_path
+    @translation_hash = {locale => translation_hash}
+    @yaml_hash = YAML::load_file(@yaml_file_path)
+    @locale = locale
+  end
+
+  def process_and_store!
+    process!
+    store
+  end
+
+  def process
+    @yaml_hash = @yaml_hash.deep_merge(@translation_hash)
+  end
+
+  def store
+    FileWriter.write(@yaml_file_path, @yaml_hash.to_yaml)
+  end
 end
+
+class Hash
+  def deep_merge(second)
+    merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
+    self.merge(second, &merger)
+  end
+end
+
+#YamlWriter.new("#{p}test.yaml",h).process_and_store!
