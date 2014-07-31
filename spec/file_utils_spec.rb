0092cd168b8a45f6605d975844714ab57beafb3c
sacry-dyn <matthias.nitsche@dynport.de>
2014-07-31 10:57:49 +0200

    enahnced file_utils spec for key generation

--- spec/file_utils_spec.rb
+++ spec/file_utils_spec.rb
@@ -17,6 +17,16 @@ describe "FileUtils" do
         subject { KeyGenerator.generate_key_base_from_file(file_path, extension) }
         it { should == "application.sidebar.show" }
       end
+      context "subdir name" do
+        let( :file_path ) { "app/views/application/sidebar/_show.html.slim"}
+        subject { KeyGenerator.subdir_name(file_path, ["views"]) }
+        it { should == "application.sidebar" }
+      end
+      context "filename" do
+        let( :file_path ) { "app/views/application/sidebar/_show.html.slim"}
+        subject { KeyGenerator.filename(file_path) }
+        it { should == "show" }
+      end
     end
     describe "rb"  do
       let( :extension ) { "rb" }
@@ -29,6 +39,16 @@ describe "FileUtils" do
         let( :file_path ) { "app/controllers/some_dir/some_controller.rb"}
         subject { KeyGenerator.generate_key_base_from_file(file_path, extension) }
         it { should == "some_dir.some_controller" }
+      end
+      context "subdir name" do
+        let( :file_path ) { "app/controllers/some_dir/some_controller.rb"}
+        subject { KeyGenerator.subdir_name(file_path, ["controllers", "models"]) }
+        it { should == "some_dir" }
+      end
+      context "filename" do
+        let( :file_path ) { "app/controllers/some_dir/some_controller.rb"}
+        subject { KeyGenerator.filename(file_path, ".rb") }
+        it { should == "some_controller" }
       end
     end
   end
